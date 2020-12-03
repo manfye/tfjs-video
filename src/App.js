682e493c91299650dd584a291f9635d86ef5e5d8
manfye <manfye@gmail.com>
2020-12-04 01:10:29 +0800

    test loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import padSequences from './helper/paddedSeq'
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import {
   TextField,
   Grid,
@@ -9,19 +8,17 @@ import {
   Toolbar,
   Typography,
   IconButton,
-  Button
+  Button,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
+import * as handpose from "@tensorflow-models/handpose";
+// import * as tf from "@tensorflow/tfjs-core";
+import * as tf from "@tensorflow/tfjs";
+import {loadGraphModel} from '@tensorflow/tfjs-converter';
 
 
-} from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu';
-import { makeStyles } from '@material-ui/core/styles';
-import { blue } from '@material-ui/core/colors';
-import * as handpose from '@tensorflow-models/handpose';
-import * as tf from '@tensorflow/tfjs-core';
-import '@tensorflow/tfjs-backend-webgl';
-import imageDataURI from 'image-data-uri'
-
-import CameraPhoto, { FACING_MODES } from 'jslib-html5-camera-photo';
 
 function App() {
   const useStyles = makeStyles((theme) => ({
@@ -38,180 +35,119 @@ function App() {
   const classes = useStyles();
 
   const url = {
+    model:
+      "https://cdn.esyms.com/emailImg/model.json",
+  };
 
-    model: 'https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/model.json',
-    metadata: 'https://storage.googleapis.com/tfjs-models/tfjs/sentiment_cnn_v1/metadata.json'
-};
+  const [inputText, setInput] = useState("");
+  const [imageData, setImageData] = useState("hand.jpg");
+  const [model, setModel] = useState();
+
+  async function loadModel(url) {
+    try {
+      const model = await tf.loadGraphModel(url.model);
+      setModel(model);
+      console.log("setloadedModel")
+    } catch (err) {
+      console.log(err);
+      console.log("failed load model")
+
+    }
 
 
-
-const [inputText, setInput] = useState("")
-const [imageData, setImageData] = useState("hand.jpg")
-
-async function loadModel(url) {
-  
-  try {
-    // Load the MediaPipe handpose model.
-  const model = await handpose.load();
-  // Pass in a video stream (or an image, canvas, or 3D tensor) to obtain a
-  // hand prediction from the MediaPipe graph.
-  predictionFunction()
-  } catch (err) {
-    console.log(err);
   }
-   
-  
-  
+
+  // function Draw(data, fullData) {
+  //   console.log(data);
+  //   var cnvs = document.getElementById("myCanvas");
+
+  //   cnvs.style.position = "absolute";
+
+  //   var ctx = cnvs.getContext("2d");
+  //   ctx.clearRect(0, 0, cnvs.width, cnvs.height);
+
+  //   fullData.landmarks.map((i) => {
+  //     ctx.beginPath();
+  //     ctx.arc(i[0], i[1], 5, 0, 2 * Math.PI, false);
+  //     ctx.strokeStyle = "#FF0000";
+
+  //     ctx.lineWidth = 3;
+  //     ctx.stroke();
+  //   });
+  // }
+
+  // async function predictionFunction() {
+  //   const model = await handpose.load();
+  //   console.log("Loading");
+
+  //   const predictions = await model.estimateHands(
+  //     document.getElementById("img")
+  //   );
+  //   if (predictions.length > 0) {
+  //     console.log(predictions);
+  //     Draw(predictions[0].annotations, predictions[0]);
+  //     for (let i = 0; i < predictions.length; i++) {
+  //       const keypoints = predictions[i].landmarks;
+
+  //       // Log hand keypoints.
+  //       for (let i = 0; i < keypoints.length; i++) {
+  //         const [x, y, z] = keypoints[i];
+  //         console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+  //       }
+  //     }
+  //   } else {
+  //     console.log(predictions);
+  //     console.log("No hand detected");
+  //     var cnvs = document.getElementById("myCanvas");
+
+  //     cnvs.style.position = "absolute";
+
+  //     var ctx = cnvs.getContext("2d");
+  //     ctx.clearRect(0, 0, cnvs.width, cnvs.height);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   predictionFunction();
+  // }, [imageData]);
+
+  useEffect(() => {
+    tf.ready().then(() => {
+      loadModel(url);
+    });
+  }, []);
+
+
+  async function  predictTuber(){
+  let tensor = tf.browser.fromPixels(document.getElementById("img"), 3)
+		.resizeNearestNeighbor([224, 224]) // change the image size
+		.expandDims()
+		.toFloat()
+		.reverse(-1); // RGB -> BGR
+	let predictions = await model.predict(tensor).data();
+	console.log(predictions);
 }
 
-
-
-function Draw(data, fullData) {
-  console.log(data);
-  var cnvs = document.getElementById("myCanvas");
-
-  cnvs.style.position = "absolute";
-
-  var ctx = cnvs.getContext("2d");
-  ctx.clearRect(0, 0, cnvs.width, cnvs.height);
-
-  fullData.landmarks.map((i) => {
-    ctx.beginPath();
-    ctx.arc(i[0], i[1], 5, 0, 2 * Math.PI, false);
-    ctx.strokeStyle = "#FF0000";
-
-    ctx.lineWidth = 3;
-    ctx.stroke();
-  });
-
-  // data.middleFinger.map((i) => {
-  //   ctx.beginPath();
-  //   ctx.arc(i[0], i[1], 5, 0, 2 * Math.PI, false);
-  //   ctx.strokeStyle = "#FF0000";
-
-  //   ctx.lineWidth = 3;
-  //   ctx.stroke();
-  // });
-
-  // data.indexFinger.map((i) => {
-  //   ctx.beginPath();
-  //   ctx.arc(i[0], i[1], 5, 0, 2 * Math.PI, false);
-  //   ctx.strokeStyle = "#FF0000";
-
-  //   ctx.lineWidth = 3;
-  //   ctx.stroke();
-  // });
-
-  // data.ringFinger.map((i) => {
-  //   ctx.beginPath();
-  //   ctx.arc(i[0], i[1], 5, 0, 2 * Math.PI, false);
-  //   ctx.strokeStyle = "#FF0000";
-
-  //   ctx.lineWidth = 3;
-  //   ctx.stroke();
-  // });
-
-  // data.thumb.map((i) => {
-  //   ctx.beginPath();
-  //   ctx.arc(i[0], i[1], 5, 0, 2 * Math.PI, true);
-  //   ctx.strokeStyle = "#FF0000";
-
-  //   ctx.lineWidth = 3;
-  //   ctx.stroke();
-  // });
-
-  
-  // data.pinky.map((i) => {
-  //   ctx.beginPath();
-  //   ctx.arc(i[0], i[1], 5, 0, 2 * Math.PI, false);
-  //   ctx.strokeStyle = "#FF0000";
-
-  //   ctx.lineWidth = 3;
-  //   ctx.stroke();
-  // });
-
-  // data.palmBase.map((i) => {
-  //   ctx.beginPath();
-  //   ctx.arc(i[0], i[1], 5, 0, 2 * Math.PI, false);
-  //   ctx.strokeStyle = "#FF0000";
-
-  //   ctx.lineWidth = 3;
-  //   ctx.stroke();
-  // });
-
-
-
-}
-
- async function predictionFunction(){
-  const model = await handpose.load();
-console.log("Loading")
-
-  const predictions = await model.estimateHands(document.getElementById('img'));
-  if (predictions.length > 0) {
-    console.log(predictions)
-Draw(predictions[0].annotations,predictions[0])
-    for (let i = 0; i < predictions.length; i++) {
-      const keypoints = predictions[i].landmarks;
-
-      // Log hand keypoints.
-      for (let i = 0; i < keypoints.length; i++) {
-        const [x, y, z] = keypoints[i];
-        console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+  const handleCapture = (target) => {
+    if (target.files) {
+      if (target.files.length !== 0) {
+        const file = target.files[0];
+        const newUrl = URL.createObjectURL(file);
+        setImageData(newUrl);
       }
     }
-  }
-  else{
-    console.log(predictions)
-    console.log("No hand detected")
-    var cnvs = document.getElementById("myCanvas");
-
-    cnvs.style.position = "absolute";
-  
-    var ctx = cnvs.getContext("2d");
-    ctx.clearRect(0, 0, cnvs.width, cnvs.height);
-  
-  }
-}
-
-useEffect(() => {
-  predictionFunction()
-}, [imageData])
-
-  
-
-
-
-
-useEffect(()=>{
-  tf.ready().then(
-    ()=>{
-      loadModel(url)
-
-    }
-  );
-
-},[])
-
-
-const handleCapture = (target) => {
-  if (target.files) {
-    if (target.files.length !== 0) {
-      const file = target.files[0];
-      const newUrl = URL.createObjectURL(file);
-      setImageData(newUrl);
-      predictionFunction()
-    
-    }
-  }}
-
+  };
 
   return (
-
-     <>
-       <AppBar position="static">
+    <>
+      <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
@@ -219,23 +155,58 @@ const handleCapture = (target) => {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Button variant={"outlined"} onClick ={()=>{setImageData("hand2.jpg")}}>Hand 2</Button>
-      <Button variant={"outlined"} onClick ={()=>{setImageData("hand.jpg")}}>Hand 1</Button>
+      <Button
+        variant={"outlined"}
+        onClick={() => {
+          setImageData("hand2.jpg");
+        }}
+      >
+        Hand 2
+      </Button>
+      <Button
+        variant={"outlined"}
+        onClick={() => {
+          setImageData("hand.jpg");
+        }}
+      >
+        Hand 1
+      </Button>
+      <Button
+        variant={"outlined"}
+        onClick={() => {
+          predictTuber()
+        }}
+      >
+        Predict
+      </Button>
+      <input
+        accept="image/*"
+        id="icon-button-file"
+        type="file"
+        capture="environment"
+        onChange={(e) => handleCapture(e.target)}
+      />
 
-      <input accept="image/*" id="icon-button-file" type="file" capture="environment" onChange={(e) => handleCapture(e.target)}/>
+      <Grid container style={{ height: "90vh" }}>
+        <img
+          style={{ width: 960, height: 540, objectFit: "fill" }}
+          id="img"
+          src={imageData}
+        ></img>
 
-      <Grid container style={{ height:"90vh", }}>
-      
-      <img style={{width:960, height: 540, objectFit: "contain"}} id="img" src={imageData}></img>
-
-<canvas id='myCanvas' width={960} height={540} style={{backgroundColor:"transparent"}} />
-
-
-
+        <canvas
+          id="myCanvas"
+          width={960}
+          height={540}
+          style={{ backgroundColor: "transparent" }}
+        />
       </Grid>
-      <Button onClick ={()=>{setImageData("hand2.jpg")}}></Button>
-     </>
-    
+      <Button
+        onClick={() => {
+          setImageData("hand2.jpg");
+        }}
+      ></Button>
+    </>
   );
 }
 
